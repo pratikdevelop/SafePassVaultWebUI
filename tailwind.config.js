bf647fff10d2ac88e09893962a6caa7bd0ac40a6
pratik <pratikraut88895@gmail.com>
2024-11-16 00:00:34 +0530

    chrome extension code updation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./src/**/*.{html,ts}",
-      './node_modules/preline/preline.js',
+      "./**/*.{html,ts}",
+      
     ],
     theme: {
       extend: {
