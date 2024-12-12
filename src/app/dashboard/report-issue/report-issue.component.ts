ed94eeb4178ebbd0eeb0aa6d585dbd5456906083
pratik <pratikraut88895@gmail.com>
2024-12-13 00:44:24 +0530

    Working on create report issue page for users

---
+++ src/app/dashboard/report-issue/report-issue.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-report-issue',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatOptionModule
+    
+
+  ],
+  templateUrl: './report-issue.component.html',
+  styleUrl: './report-issue.component.css'
+})
+export class ReportIssueComponent {
+
+}
